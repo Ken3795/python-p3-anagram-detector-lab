776cb05483be48a77cb5fd4cfccc8f434ca6acf5
Kenneth Kipkoech <kenkipkoech57@gmail.com>
2025-02-15 15:36:54 +0300

    lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        '''Initializes with a single word.'''
+        self.word = word.lower()
+
+    def match(self, word_list):
+        '''Returns a list of words that are anagrams of the initialized word.'''
+        sorted_word = sorted(self.word)
+        anagrams = []
+
+        for candidate in word_list:
+            if sorted_word == sorted(candidate.lower()):
+                anagrams.append(candidate)
+
+        return anagrams
